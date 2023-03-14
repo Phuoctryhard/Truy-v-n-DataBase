6361e89d8bcf6c571400468746e6aae0a5cc2ada
Phuoctryhard <ngodinhphuoc100@gmail.com>
2023-03-14 16:25:59 +0700

    updateDatabase

---
+++ UpdateDatabase/Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace UpdateDatabase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Idma = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textDVT = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(29, 267);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "dung parameter";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(176, 267);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(137, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "no use para";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.textDVT);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Location = new System.Drawing.Point(299, 125);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(302, 325);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "chỉnh sửa";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.listView1);
+            this.groupBox2.Location = new System.Drawing.Point(0, 125);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(293, 325);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Thông tin món ăn";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(107, 33);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(185, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(107, 84);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(185, 22);
+            this.textBox3.TabIndex = 5;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(107, 133);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(185, 22);
+            this.textBox4.TabIndex = 6;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(107, 239);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(185, 22);
+            this.textBox5.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 16);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Id monan";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(-3, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 16);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Id nhom mon an";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 16);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Ten mon";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(35, 239);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 16);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "don gia";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(68, 68);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(121, 16);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Chon nhom mon an";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Idma,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(3, 18);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(287, 295);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // Idma
+            // 
+            this.Idma.Text = "Id ma";
+            this.Idma.Width = 80;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Id nhóm ma";
+            this.columnHeader2.Width = 80;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Tên Ma";
+            this.columnHeader3.Width = 80;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "ĐV tính";
+            this.columnHeader4.Width = 80;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Đơn giá";
+            this.columnHeader5.Width = 80;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(255, 65);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 13;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textDVT
+            // 
+            this.textDVT.Location = new System.Drawing.Point(107, 180);
+            this.textDVT.Name = "textDVT";
+            this.textDVT.Size = new System.Drawing.Size(185, 22);
+            this.textDVT.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(26, 180);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(66, 16);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "đơn vị tính";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader Idma;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textDVT;
+    }
+}
+
---
+++ UpdateDatabase/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UpdateDatabase
+{
+    public partial class Form1 : Form
+    {
+
+        // link 
+        string strcon = @"Data Source=DESKTOP-M4UKRGH\SQLEXPRESS01;Initial Catalog=QLNH;Integrated Security=True";
+        SqlConnection sqlcon = null;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            if (sqlcon == null)
+            {
+                sqlcon = new SqlConnection(strcon);
+            }
+            if (sqlcon.State != ConnectionState.Open)
+            {
+                sqlcon.Open();
+                MessageBox.Show("Thanh cong");
+
+            }
+            SqlCommand sqlcmd = new SqlCommand();
+            sqlcmd.CommandType = CommandType.Text;
+            sqlcmd.CommandText = "select * from NhomMonAn1";
+
+            sqlcmd.Connection = sqlcon;
+
+            SqlDataReader reader = sqlcmd.ExecuteReader();
+            while (reader.Read())
+            {
+                string IdNhomMonAn = reader.GetString(0);
+                string TenNhomMonAn = reader.GetString(1);
+
+                string line = IdNhomMonAn.Trim() + "/" + TenNhomMonAn;
+                comboBox1.Items.Add(line);
+
+
+            }
+            reader.Close();
+        }
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+
+
+
+        // khong dung parameter 
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // nhom1/khaivi 
+            if (comboBox1.SelectedIndex == -1)
+            {
+                return;
+            }
+            string aa = comboBox1.SelectedItem.ToString();
+            string[] mang = aa.Split('/');
+            string dau = mang[0].Trim();
+            // nhom01
+            // muon hien thi trogn listview;
+            hienthithongtin(dau);
+        }
+        private void hienthithongtin(string manhom)
+        {
+
+            if (sqlcon == null)
+            {
+                sqlcon = new SqlConnection(strcon);
+            }
+            if (sqlcon.State != ConnectionState.Open)
+            {
+                sqlcon.Open();
+                MessageBox.Show("Thanh cong");
+
+            }
+            SqlCommand sqlcmd = new SqlCommand();
+            sqlcmd.CommandType = CommandType.Text;
+            sqlcmd.CommandText = "select * from MonAn1 Where IdNhomMonAn = '"+manhom+"'";
+
+            sqlcmd.Connection = sqlcon;
+            listView1.Items.Clear();
+            SqlDataReader reader = sqlcmd.ExecuteReader();
+            while (reader.Read())
+            {
+                string idmonan = reader.GetString(0);
+                string idnhommonan = reader.GetString(1);
+                string namemonan = reader.GetString(2);
+                string donvitinh = reader.GetString(3);
+                float dongia = reader.GetFloat(4);
+                // tao ra  hang 
+                ListViewItem hang = new ListViewItem(idmonan);
+                hang.SubItems.Add(idnhommonan);
+                hang.SubItems.Add(namemonan);
+                hang.SubItems.Add(donvitinh);
+                hang.SubItems.Add(dongia.ToString());
+                listView1.Items.Add(hang);
+
+            }
+            reader.Close();
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // lay ra hàng chỉ định 
+            if(listView1.SelectedItems.Count > 0)
+            {
+                // lay ra hang do 
+                ListViewItem hang = listView1.SelectedItems[0];
+                textBox2.Text = hang.SubItems[0].Text.Trim();
+                textBox3.Text = hang.SubItems[1].Text.Trim();
+                textBox4.Text = hang.SubItems[2].Text.Trim();
+                textBox5.Text = hang.SubItems[4].Text.Trim();
+                textDVT.Text = hang.SubItems[3].Text;
+
+
+            }
+        }
+
+        // dung parameter 
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (sqlcon == null)
+            {
+                sqlcon = new SqlConnection(strcon);
+
+            }
+            if (sqlcon.State != ConnectionState.Open)
+            {
+                sqlcon.Open();
+
+            }
+            //
+            SqlCommand sqlcmd = new SqlCommand();
+            sqlcmd.CommandType = CommandType.Text;
+            sqlcmd.CommandText = "update MonAn1 set IdMonAn=@monan,IdNhomMonAn=@nhommonan,NameMonAn =@tenmon,DonViTinh=@donvi,DonGia=@dongia where IdMonAn=@monan";
+
+            // dinh nghia parameter 
+            SqlParameter MA= new SqlParameter("monan", SqlDbType.VarChar);
+            SqlParameter NhomMA = new SqlParameter("nhommonan", SqlDbType.VarChar);
+            SqlParameter NameM = new SqlParameter("tenmon", SqlDbType.VarChar);
+            SqlParameter DonVi1 = new SqlParameter("donvi", SqlDbType.VarChar);
+            SqlParameter DonGia1 = new SqlParameter("dongia", SqlDbType.Real);
+
+            MA.Value = textBox2.Text.Trim();
+            NhomMA.Value = textBox3.Text.Trim();
+            NameM.Value = textBox4.Text.Trim();
+            DonVi1.Value = textDVT.Text.Trim();
+            DonGia1.Value = textBox5.Text.Trim();
+
+            // dua vao sql 
+            sqlcmd.Parameters.Add(MA);
+            sqlcmd.Parameters.Add(NhomMA);
+            sqlcmd.Parameters.Add(NameM);
+            sqlcmd.Parameters.Add(DonVi1);
+            sqlcmd.Parameters.Add(DonGia1);
+
+            //ket noi
+            sqlcmd.Connection = sqlcon;
+            int kq = sqlcmd.ExecuteNonQuery();
+
+            if (kq > 0)
+            {
+                hienthithongtin(textBox3.Text.Trim());
+            }
+            else
+            {
+                MessageBox.Show("ko đc ");
+            }
+
+        }
+
+    }
+}
